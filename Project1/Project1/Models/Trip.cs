e6ca3832325216ce8d18cc9e18adc87f387a0ddd
s00634492 <44267767+s00634492@users.noreply.github.com>
2021-03-17 04:26:30 -0400

    Add files via upload

---
+++ Project1/Project1/Models/Trip.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class Trip
+    {
+        public int TripID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a destination.")]
+        public string Destination { get; set; }
+        [Required(ErrorMessage = "Please enter a start date.")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Please enter an end date.")]
+        public DateTime EndDate { get; set; }
+        public string Accommodation { get; set; }
+
+        public string AccommodationPhone { get; set; }
+        public string AccommodationEmail { get; set; }
+
+        public string ThingToDo1 { get; set; }
+        public string ThingToDo2 { get; set; }
+        public string ThingToDo3 { get; set; }
+    }
+}
---
+++ Project1/Project1/Models/TripContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project1.Models
+{
+    public class TripContext : DbContext
+    {
+        public TripContext(DbContextOptions<TripContext> options) : base(options) { }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trip>().HasData(
+                new Trip{
+                    TripID = 1,
+                    Destination = "Cleveland",
+                    StartDate = new DateTime(2021, 3, 16, 0, 0, 0, 0),
+                    EndDate = new DateTime(2021, 3, 17, 0, 0, 0, 0),
+                    Accommodation = "Kingston Hotel",
+                    AccommodationPhone = "5551234",
+                    AccommodationEmail = "help@kingston.com",
+                    ThingToDo1 = "Eat cake"
+                },new Trip{
+                    TripID = 2,
+                    Destination = "Miami",
+                    StartDate = new DateTime(2021, 3, 18, 0, 0, 0, 0),
+                    EndDate = new DateTime(2021, 3, 19, 0, 0, 0, 0),
+                    Accommodation = "Beachy Hotel",
+                    AccommodationPhone = "5554312",
+                    AccommodationEmail = "help@beachy.com",
+                    ThingToDo1 = "Swim in ocean"
+                },new Trip{
+                    TripID = 3,
+                    Destination = "Las Vegas",
+                    StartDate = new DateTime(2021, 3, 20, 0, 0, 0, 0),
+                    EndDate = new DateTime(2021, 3, 21, 0, 0, 0, 0),
+                    Accommodation = "Valley Hotel",
+                    AccommodationPhone = "5551212",
+                    AccommodationEmail = "help@valley.com",
+                    ThingToDo1 = "Visit Area51",
+                    ThingToDo2 = "See a show"
+                });
+        }
+    }
+}
