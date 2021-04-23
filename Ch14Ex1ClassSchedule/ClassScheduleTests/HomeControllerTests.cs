a44e18a09d42dd58683de0d243c5e699ea083f27
s00634492 <44267767+s00634492@users.noreply.github.com>
2021-04-23 01:12:51 -0400

    Add files via upload

---
+++ Ch14Ex1ClassSchedule/ClassScheduleTests/HomeControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using ClassSchedule.Models;
+using ClassSchedule.Controllers;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace ClassScheduleTests
+{
+    public class HomeControllerTests
+    {
+        private IClassScheduleUnitOfWork GetUnitOfWork()
+        {
+            //days teachers classes
+            var dayRep = new Mock<IRepository<Day>>();
+            dayRep.Setup(m => m.List(It.IsAny<QueryOptions<Day>>())).Returns(new List<Day>());
+
+            var teacherRep = new Mock<IRepository<Teacher>>();
+            teacherRep.Setup(m => m.List(It.IsAny<QueryOptions<Teacher>>())).Returns(new List<Teacher>());
+
+            var classRep = new Mock<IRepository<Class>>();
+            classRep.Setup(m => m.List(It.IsAny<QueryOptions<Class>>())).Returns(new List<Class>());
+
+            var unit = new Mock<IClassScheduleUnitOfWork>();
+            unit.Setup(m => m.Days).Returns(dayRep.Object);
+            unit.Setup(m => m.Teachers).Returns(teacherRep.Object);
+            unit.Setup(m => m.Classes).Returns(classRep.Object);
+            return unit.Object;
+        }
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IndexActionMethod_ReturnsAViewResult(int n1)
+        {
+            // arrange
+            var accessor = new Mock<IHttpContextAccessor>();
+            var context = new DefaultHttpContext();
+
+            accessor.Setup(m => m.HttpContext).Returns(context);
+            accessor.Setup(m => m.HttpContext.Request).Returns(context.Request);
+            accessor.Setup(m => m.HttpContext.Response).Returns(context.Response);
+            accessor.Setup(m => m.HttpContext.Request.Cookies).Returns(context.Request.Cookies);
+            accessor.Setup(m => m.HttpContext.Response.Cookies).Returns(context.Response.Cookies);
+
+            var session = new Mock<ISession>();
+            accessor.Setup(m => m.HttpContext.Session).Returns(session.Object);
+
+            var unit = GetUnitOfWork();
+            var controller = new HomeController(unit, accessor.Object);
+
+            // act
+            var result = controller.Index(n1);
+
+            // assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
