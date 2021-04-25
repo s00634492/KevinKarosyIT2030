f15e8a64a40bfa7e8d10dd66804a467c848a55e5
s00634492 <44267767+s00634492@users.noreply.github.com>
2021-04-25 18:09:34 -0400

    Add files via upload

---
+++ Project3/Project2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new PriceQuotation());
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotation priceQuote)
+        {
+            return View(priceQuote);
+        }
+    }
+}
