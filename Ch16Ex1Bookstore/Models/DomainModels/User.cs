6ef8e5a83fa502435551d1bc9e412b96d36a7aa2
s00634492 <44267767+s00634492@users.noreply.github.com>
2021-05-05 23:52:08 -0400

    Add files via upload

---
+++ Ch16Ex1Bookstore/Models/DomainModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bookstore.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+    }
+}
